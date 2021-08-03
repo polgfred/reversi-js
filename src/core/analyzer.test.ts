fdbe8606630a975a53c6961916bd5054e3d39422
Frederick Polgardy <fred@polgardy.com>
2021-08-03 09:25:51 -0600

    clean up analyzer

--- src/core/analyzer.test.ts
+++ src/core/analyzer.test.ts
@@ -6,7 +6,9 @@ const { BLACK } = SideType;
 
 describe('Analyzer', () => {
   it('should run the evaluator on this position', () => {
-    const [, score] = analyze(newBoard(), BLACK, 0);
-    expect(score).toEqual(0);
+    const [move, score] = analyze(newBoard(), BLACK, 1);
+    // brittle
+    expect(move).toEqual([3, 2, 0, 0, 0, 0, 0, 0, 1, 0]);
+    expect(score).toEqual(40);
   });
 });
--- src/core/analyzer.ts
+++ src/core/analyzer.ts
@@ -12,6 +12,12 @@ export function analyze(
   side: SideType,
   level: number = LEVEL
 ): readonly [MoveType, number] {
+  // make sure level is valid
+  if (level < 1) {
+    throw new Error('level < 1');
+  }
+
+  // get the rules
   const { getBoard, getSide, findMoves, doMove } = makeRules(board, side);
   // start the descent
   return loop(level);
@@ -20,31 +26,25 @@ export function analyze(
   function loop(level: number) {
     const board = getBoard();
     const side = getSide();
-    let bestScore = -side / 0;
+    // keep track of best move and score so far
     let bestMove: MoveType;
-
-    if (level > 0) {
-      // analyze counter-moves from this position
-      const moves = findMoves();
-      for (const move of moves) {
-        // perform the move and descend a level
-        const reverse = doMove(move);
-        const [, current] = loop(level - 1);
-        reverse();
-        // keep track of the best move from this position
-        if (
-          (side === BLACK && current > bestScore) ||
-          (side === WHITE && current < bestScore)
-        ) {
-          bestMove = move;
-          bestScore = current;
-        }
+    let bestScore = -side / 0;
+    // analyze counter-moves from this position
+    for (const move of findMoves()) {
+      // perform the move and descend a level
+      const reverse = doMove(move);
+      const current = level === 1 ? evaluate(board) : loop(level - 1)[1];
+      reverse();
+      // check if this was a better move
+      if (
+        (side === BLACK && current > bestScore) ||
+        (side === WHITE && current < bestScore)
+      ) {
+        bestMove = move;
+        bestScore = current;
       }
-    } else {
-      // we've hit bottom so just return the score for this position
-      bestScore = evaluate(board);
     }
-    // a pair representing the winning move and score for this turn
+    // the winning move and score for this turn
     return [bestMove, bestScore] as const;
   }
 }
