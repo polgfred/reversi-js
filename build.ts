0b0c7a572df6ae80094cec83b6fd92751d6e1695
Frederick Polgardy <fred@polgardy.com>
2025-12-16 21:49:55 +0000

    remove nonexistent plugin

--- build.ts
+++ build.ts
@@ -1,10 +1,8 @@
 import Bun from 'bun';
-import reactSvgPlugin from './plugin-react-svg.js';
 
 await Bun.build({
   entrypoints: ['./src/index.html', './src/worker.ts'],
   format: 'esm',
   outdir: './dist',
   minify: true,
-  plugins: [reactSvgPlugin],
 });
